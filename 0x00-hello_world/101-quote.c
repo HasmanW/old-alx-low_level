36369523609450416c6192ef1076d9507250bb48
The_Tutor <hasmanwasilwaw@gmail.com>
2022-09-09 06:15:02 +0300

    Corrections and Additions

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,10 @@
 /**
  * main - starts the program
  *
- * Return: returns program output
+ * Return: returns 1
  */
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
 }
