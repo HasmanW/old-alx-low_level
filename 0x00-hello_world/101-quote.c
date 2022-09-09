29bb4b747c3495bd0e28f6062dbfa3a6b1690eae
The_Tutor <hasmanwasilwaw@gmail.com>
2022-09-09 05:58:55 +0300

    New update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - starts the program
+ *
+ * Return: returns program output
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+}
