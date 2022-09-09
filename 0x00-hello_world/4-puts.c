94ec11e0c60c1e174076d7ea3c091a5a817ca7e5
The_Tutor <hasmanwasilwaw@gmail.com>
2022-09-09 03:36:48 +0300

    New commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
 /*The main program starts*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
