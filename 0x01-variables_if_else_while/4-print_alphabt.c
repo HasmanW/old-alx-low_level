ae5f60812daacafb4ef6e662c8ebf37df4831ad9
The_Tutor <hasmanwasilwaw@gmail.com>
2022-09-10 05:54:50 +0300

    New Commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,10 +8,17 @@
 int main(void)
 {
 	int i;
+	int e = 'e';
+	int q = 'q';
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
+		if (i == e || i == q)
+		{
+			continue;
+		}
 		putchar(i);
 	}
-	putchar('\n');
+	putchar ('\n');
+	return (0);
 }
