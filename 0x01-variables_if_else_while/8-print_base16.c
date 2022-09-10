40b31a4f278fed6d7e98c3e2ae7196007838190f
The_Tutor <hasmanwasilwaw@gmail.com>
2022-09-10 04:22:10 +0300

    New push

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,4 +13,5 @@ int main(void)
 	{
 		printf("%x", i);
 	}
+	return (0);
 }
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Starts the program
+ *
+ * Return - Returns main output
+*/
+int main(void)
+{
+	int i;
+
+	char* str = '0', '1', '2', '3', '4', '5', '6', '7', '8', '9';
+
+}
