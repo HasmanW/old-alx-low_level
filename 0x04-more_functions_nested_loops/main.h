55de8d809ae25a003ab6ff83c2118eae12256ba0
The_Tutor <hasmanwasilwaw@gmail.com>
2022-09-16 03:13:54 +0300

    Task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifdef MAIN_H
+#define MAIN_H
+
+int_isupper(int c);
+int_isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void prnt_diagonal(int n);
+
+#endif
