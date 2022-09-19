f088fcab29d6ec260f244606c34f1e0d3df8e9cc
The_Tutor <hasmanwasilwaw@gmail.com>
2022-09-20 02:18:58 +0300

    First commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,8 @@
 
 int _strlen(char *s)
 {
-	for(int i = 0; s[i] != '\0'; i++)
+	int i;
+	for(i = 0; s[i] != '\0'; i++)
 	{
 		
 	}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,3 +4,5 @@
 int _putchar(char);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
+
+#endif
