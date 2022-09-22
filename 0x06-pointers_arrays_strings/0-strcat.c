9dae11d95d776cf56b2328382753543457877d97
The_Tutor <hasmanwasilwaw@gmail.com>
2022-09-22 16:13:42 +0300

    Task 0 Test

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,10 +6,7 @@
  * @*src: source variable
  * Return: returns the string
  */
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)  //we are writing a function
 {
-	*dest = &dest;
-	*src = &src;
-	char *s = *dest + *src;
-	return s;
+	_putchar(dest, src);
 }
