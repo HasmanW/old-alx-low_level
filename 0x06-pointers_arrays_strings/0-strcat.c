b79baa99a5287c35df30866266a621de7adf519d
The_Tutor <hasmanwasilwaw@gmail.com>
2022-09-22 15:16:47 +0300

    Task 0 Test

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,11 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings 
+ * @*dest: destination variable
+ * @*src: source variable
+ * Return: returns the string
+ */
 char *_strcat(char *dest, char *src)
 {
 	*dest = &dest;
