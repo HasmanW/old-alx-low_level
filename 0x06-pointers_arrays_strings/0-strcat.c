bae78fff4d4b233944298b029b30c89bf5a47dfc
The_Tutor <hasmanwasilwaw@gmail.com>
2022-09-23 05:41:52 +0300

    Task Coreections

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include <string.h>
+
 /**
- * *_strcat - concatenates two strings 
- * @*dest: destination variable
- * @*src: source variable
- * Return: returns the string
+ * _strcat - concatenates two strings
+ * @dest: pointer to char
+ * @src: pointer to char
+ * Return: concantinated pointer string
  */
-char *_strcat(char *dest, char *src)  //we are writing a function
+char *_strcat(char *dest, char *src)
 {
 	strcat(dest, src);
+	return (dest);
 }
