f83b82443d2b6a6e25fdb4cfbe1bf5aadb67e978
The_Tutor <hasmanwasilwaw@gmail.com>
2022-09-27 02:04:12 +0300

    Task 2

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - a function that fills memory with a constant byte
+ * @*s:
+ * @b:
+ * @n:
+ *
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	char memset(char *s, char b, unisgned int n);
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: string parameter
+ * @c: character in string to be located
+ * Return: returns a string
+*/
+char *_strchr(char *s, char c)
+{
+	char *p;
+
+	p = strchr(s, c);
+}
