dd07051238ff267ce925c8a563cf96b4fec25596
The_Tutor <hasmanwasilwaw@gmail.com>
2022-09-28 00:45:42 +0300

    Task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * @brief Set the string object
+ * 
+ * @s: parameter 1, pointer to pointer 
+ * @to: parameter 2,pointer to char
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
