ea759300c778f5fd97cb2f72f7a4234fd3dfcd26
The_Tutor <hasmanwasilwaw@gmail.com>
2022-09-29 10:54:03 +0300

    Tasks update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
+/**
+ * _sqrt_recursion - caculates the sqrt
+ * @n: parameter int n
+ * Return: returns an int
+ */
 int _sqrt_recursion(int n)
 {
-	
+	return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - _sqrt_recursion
+ * @n: first parameter
+ * @i: second parameter
+ * Return: returns sqrt
+ */
+int _sqrt(int n, int i)
+{
+	if (n < 0)
+		return (-1);
+	if ((i * i) > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
 }
