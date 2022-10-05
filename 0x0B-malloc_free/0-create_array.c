c1f808f7e60d9cf0fbbd521e8fd03efa53e73c8b
The_Tutor <hasmanwasilwaw@gmail.com>
2022-10-05 05:25:25 +0300

    Task Update

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - Create a array object
+ *
+ * @size: First parameter
+ * @c: Second parameter
+ * Return: returns an array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int n;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	s = malloc(size * (sizeof(c));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (n = 0; n < size; n++)
+	{
+		s[n] = c;
+	}
+	return (s);
+
+}
