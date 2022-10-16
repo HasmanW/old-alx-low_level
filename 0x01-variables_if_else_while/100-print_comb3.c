0cb4c59714e1ee43212233954c49b7cb8eed41b8
The_Tutor <hasmanwasilwaw@gmail.com>
2022-10-16 21:46:48 +0300

    update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * main - prints a combination of two numbers
+ *
+ * Return: returns true
+ */
+int main(void)
+{
+	/*declare variables*/
+	int a, b;
+
+	/*print the integers*/
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			putchar(a);
+			putchar(b);
+
+			if (a != 56 || b != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+}
+
