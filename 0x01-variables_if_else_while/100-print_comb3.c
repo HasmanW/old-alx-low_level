f54fb8e8d796c0921c771eaf3a80dba8e5bf77d0
The_Tutor <hasmanwasilwaw@gmail.com>
2022-10-16 22:29:10 +0300

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,10 +13,15 @@ int main(void)
 	/*print the integers*/
 	for (a = 48; a < 57; a++)
 	{
-		for (b = 49; b <= 57; b++)
+		for (b = a + 1; b <= 57; b++)
 		{
 			putchar(a);
 			putchar(b);
+
+			if (a == 57 && b == 56)
+			{
+				break;
+			}
 
 			if (a != 56 || b != 57)
 			{
