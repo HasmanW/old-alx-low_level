cbc7f4fd6a834903040ac9cb7c2896fbd54c275b
The_Tutor <hasmanwasilwaw@gmail.com>
2022-10-25 05:37:16 +0300

    Tasks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,30 +1,3 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
-
-@HasmanW
-mz-jennytech
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x12-singly_linked_lists/0-print_list.c
-@mz-jennytech
-mz-jennytech task 0
-Latest commit 4975b79 4 days ago
- History
- 1 contributor
-26 lines (21 sloc)  420 Bytes
-
 #include "lists.h"
 /**
  * print_list - prints all the elements of `list_t` list
@@ -39,11 +12,13 @@ size_t print_list(const list_t *h)
 	{
 
 		if (h->str == NULL)
-		/*if false i.e. NULL, NaN, undefined or "" */
-			printf("[0] (nil)\n");
+        {
+            printf("[0] (nil)\n");
+        }
 		else
-			printf("[%u] %s\n", h->len, h->str);
-
+        {
+            printf("[%u] %s\n", h->len, h->str);
+        }
 
 		h = h->next;
 		s++;
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,10 +8,10 @@
 typedef struct lists_s
 {
     char  *str;
-    unsigned int leng;
+    unsigned int len;
     struct list_s *next;
 }list_t;
 
-size_t print_list(const list_t *h)
+size_t print_list(const list_t *h);
 
 #endif
