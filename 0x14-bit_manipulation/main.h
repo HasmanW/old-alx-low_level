b14586f916c632568db912f2682dfee7e7f93076
The_Tutor <hasmanwasilwaw@gmail.com>
2022-11-02 01:42:48 +0300

    update 2

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,8 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <limits.h>
 
 unsigned int binary_to_uint(const char *b);
 int check_string (const char *b);
+void print_binary(unsigned long int n);
 
 #endif
