f6ec3603a35ae9b4fb55ca28beb0d1b03ce29e90
The_Tutor <hasmanwasilwaw@gmail.com>
2022-11-02 02:12:12 +0300

    Tasks Update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,46 +1,53 @@
 #include "main.h"
+
 /**
- * binary_to_uint - converts a binary to an unsigned int
- * @b: the binary number as a string
- *
- * Return: the converted value
+ * binary_to_uint - converts binary number to unsigned int
+ * @b: string of 0s and 1s
+ * Return: returns the converted unsigned int or 0 if
+ *         there is one or more char in string b that is not 0 or 1
+ *         b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int decimal = 0;
-int str_len = 0, base = 1;
+	/*declaration and initialization of variables*/
+	unsigned int decimal_num = 0;
+	int base = 1, str_length = 0;
 
-if (!check_valid_string(b))
-return (0);
+	/*check if string is valid*/
+	if (!check_string(b))
+		return (0);
 
-while (b[str_len] != '\0')
-str_len++;
+	/*length string*/
+	while (b[str_length] != '\0')
+		str_length++;
 
-while (str_len)
-{
-decimal += ((b[str_len - 1] - '0') * base);
-base *= 2;
-str_len--;
-}
-return (decimal);
+	/*convertion to decimal*/
+	while (str_length)
+        {
+                decimal_num += ((b[str_len - 1] - '0') * base);
+                base *= 2;
+                str_leng--;
+        }
+        return (decimal_num);
 }
 
 /**
- * check_valid_string - checks if a string has only 0's and 1's
- * @b: string to be checked
- *
- * Return: 1 if string is valid, 0 otherwise
+ * check_string - checks if string provided is valid
+ * @b: strings of 0s and 1s
+ * Return: returns true or false
  */
-int check_valid_string(const char *b)
+int check_string(const char *b)
 {
-if (b == NULL)
-return (0);
+	/*checks if string if null*/
+	if (b == NULL)
+		return (0);
 
-while (*b)
-{
-if (*b != '1' && *b != '0')
-return (0);
-b++;
-}
-return (1);
+	/*checks if string contains anything else apart from 1s and 0s*/
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+			b++;
+	}
+	return (1);
 }
