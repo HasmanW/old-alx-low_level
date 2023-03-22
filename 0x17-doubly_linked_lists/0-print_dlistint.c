3a7434c6bac5889d18573d9938c3f780e191928f
The_Tutor <hasmanwasilwaw@gmail.com>
2023-03-22 05:36:51 +0300

    link list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - prints a doubly linked list
+ * @h: head of doubly linked list
+ * Return: returns number of nodes in the list
+ */
+size_t print_dlistint(const_t *h){
+	size_t count = 0;
+	const dlistint_t *temp;
+
+	temp = h;
+	while temp{
+		print("%d\n", temp->n);
+		temp = temp->next;
+		count += 1;
+	}
+	return count;
+}
+
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is my linked list readme
