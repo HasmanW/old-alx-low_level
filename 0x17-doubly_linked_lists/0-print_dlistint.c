e388d8abc2d8b5dc8559cfe0af8de914be4db243
The_Tutor <hasmanwasilwaw@gmail.com>
2023-03-22 05:50:22 +0300

    task update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,15 +3,19 @@
 
 /**
  * print_dlistint - prints a doubly linked list
+ *
  * @h: head of doubly linked list
+ * 
  * Return: returns number of nodes in the list
  */
-size_t print_dlistint(const_t *h){
+size_t print_dlistint(const_t *h)
+{
 	size_t count = 0;
 	const dlistint_t *temp;
 
 	temp = h;
-	while temp{
+	while (temp != NULL)
+	{
 		print("%d\n", temp->n);
 		temp = temp->next;
 		count += 1;
